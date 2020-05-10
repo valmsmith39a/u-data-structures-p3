3e3c1f5af0d88739a81f19cda7afd331f58fb25e
valmsmith39a <valmsmith39a@gmail.com>
2020-05-10 01:46:15 -0700

    rearrange array elements: quicksort, create 2 values to produce max sum

---
+++ 3_rearrange.py
@@ -0,0 +1,89 @@
+def rearrange_digits(input_list):
+    """
+    Rearrange Array Elements so as to form two number such that their sum is maximum.
+
+    Args:
+       input_list(list): Input List
+    Returns:
+       (int),(int): Two maximum sums
+    """
+    # sort list and arrange in descending order
+    quicksort(input_list)
+    mid_index = len(input_list) // 2
+    sublist_1 = [None for x in range(mid_index)]
+    sublist_2 = [None for x in range(len(input_list) - mid_index)]
+
+    index = 0
+    index_1 = len(sublist_1) - 1
+    index_2 = len(sublist_2) - 1
+
+    while index < len(input_list):
+        if (index == 0):
+            sublist_2[index_2] = input_list[index]
+            index_2 -= 1
+            index += 1
+
+        if sublist_1[index_1] == None:
+            sublist_1[index_1] = input_list[index]
+            index_1 -= 1
+            index += 1
+
+        if sublist_2[index_2] == None:
+            sublist_2[index_2] = input_list[index]
+            index_2 -= 1
+            index += 1
+
+    num1 = int("".join(map(str, sublist_1)))
+    num2 = int("".join(map(str, sublist_2)))
+
+    return [num1, num2]
+
+
+def partition(items, begin_index, end_index):
+    left_index = 0
+    pivot_index = end_index
+
+    while left_index != pivot_index:
+
+        if items[left_index] < items[pivot_index]:
+            left_index += 1
+            continue
+        # store left index item
+        # move item at pivot_index - 1 to left index
+        # move pivot item to pivot_index - 1
+        # move left index item to pivot_index
+        # update pivot_index
+        item = items[left_index]
+        items[left_index] = items[pivot_index - 1]
+        items[pivot_index - 1] = items[pivot_index]
+        items[pivot_index] = item
+        pivot_index -= 1
+
+    return pivot_index
+
+
+def sort_all(items, begin_index, end_index):
+    if end_index <= begin_index:
+        return
+
+    pivot_index = partition(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index - 1)
+    sort_all(items, pivot_index + 1, end_index)
+
+
+def quicksort(items):
+    sort_all(items, 0, len(items) - 1)
+
+
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function(test_case)
